e3fd6964759954ba994ce6c5be0c46995ff01c19
Jose Daniel <josedanq100@gmail.com>
2019-08-02 12:27:43 -0400

    first subscribe

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const WebSocket = require('ws')
+const readline = require('readline')
 
 var wsBitmexURI = 'wss://www.bitmex.com/realtime'
 var wsConnection = new WebSocket(wsBitmexURI)
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
 wsConnection.onopen = function () {
   console.log('socket connection opened properly')
-  wsConnection.send('Hello World') // send a message
+  // wsConnection.send('Hello World') // send a message
+
+  // rl.question('Please insert the command that do you want send: ', (answer) => {
+  //   wsConnection.send(`"${answer}"`)
+  // })
+
+  wsConnection.send(JSON.stringify({ op: 'subscribe', 'args': ['orderBookL2_25:XBTUSD'] }))
   console.log('message sent')
 }
 
